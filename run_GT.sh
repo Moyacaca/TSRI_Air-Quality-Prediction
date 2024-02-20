505124fb8a88a85566ad86464ed359fc27ce6dd8
Moyacaca <117159970+Moyacaca@users.noreply.github.com>
2024-02-20 10:24:32 +0800

    Update run_GT.sh

--- run_GT.sh
+++ run_GT.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+echo "Crawling the file..."
+
+cd web_crawler
+python web_interactor.py
+
+cd ..
 
 cd pred_today_GT
 
 echo "Loading the file..."
 
-python build_train.py -f "${1}" -d "${2}"
+python build_train_e2e.py
 echo "Start evaluating!"
 
 python lstm_on_web_1to1_3Day.py 
-
