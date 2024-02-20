7ce4957538527384226722f4d76a0a3648b265e8
Moyacaca <117159970+Moyacaca@users.noreply.github.com>
2024-02-20 10:24:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+echo "Crawling the file..."
+
+cd web_crawler
+python web_interactor.py
+
+cd ..
 cd pred_today
 
 echo "Loading the file..."
 
-python build_train_web.py -f "${1}" -d "${2}"
+python build_train_web.py
 echo "Start evaluating!"
 
-python lstm_on_web_1to1_3Day.py
+python lstm_on_web_1to1_3Day.py 
